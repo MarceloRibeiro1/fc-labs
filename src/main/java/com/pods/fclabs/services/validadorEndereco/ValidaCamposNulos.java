a81703f01e3c129fb5c5693f9a108a9551ef8c43
Marcelo Henrique Ribeiro <mghenr@gmail.com>
2023-03-07 16:13:19 -0300

    refactor validation

--- src/main/java/com/pods/fclabs/services/validadorEndereco/ValidaCamposNulos.java
+++ src/main/java/com/pods/fclabs/services/validadorEndereco/ValidaCamposNulos.java
@@ -10,41 +10,21 @@ import static java.util.Objects.isNull;
 public class ValidaCamposNulos implements ValidadorEndereco {
     @Override
     public void valida(Endereco endereco) {
-        validaCep(endereco.getCep(), "Campo Cep é obrigatório para cadastro do Endereço");
-        validaLogadouro(endereco.getLogadouro(), "Campo Logadouro é obrigatório para cadastro do Endereço");
+        validaCampo(endereco.getCep(), "Campo Cep é obrigatório para cadastro do Endereço");
+        validaCampo(endereco.getLogadouro(), "Campo Logadouro é obrigatório para cadastro do Endereço");
         validaMorada(endereco.getMorada(), "Campo Morada é obrigatório para cadastro do Endereço");
-        validaNumero(endereco.getNumero(), "Campo Numero é obrigatório para cadastro do Endereço");
-        validaBairro(endereco.getBairro(), "Campo Bairro é obrigatório para cadastro do Endereço");
-        validaCidade(endereco.getCidade(), "Campo Cidade é obrigatório para cadastro do Endereço");
+        validaCampo(endereco.getNumero(), "Campo Numero é obrigatório para cadastro do Endereço");
+        validaCampo(endereco.getBairro(), "Campo Bairro é obrigatório para cadastro do Endereço");
+        validaCampo(endereco.getCidade(), "Campo Cidade é obrigatório para cadastro do Endereço");
     }
 
-    private void validaCep(String campo, String mensagemException) {
-        if (isNull(campo) || campo.isEmpty()) {
-            throw new EnderecoInvalidoException(mensagemException);
-        }
-    }
-    private void validaLogadouro(String campo, String mensagemException) {
+    private void validaCampo(String campo, String mensagemException) {
         if (isNull(campo) || campo.isEmpty()) {
             throw new EnderecoInvalidoException(mensagemException);
         }
     }
     private void validaMorada(Morada campo, String mensagemException) {
         if (isNull(campo)) {
-            throw new EnderecoInvalidoException(mensagemException);
-        }
-    }
-    private void validaNumero(String campo, String mensagemException) {
-        if (isNull(campo) || campo.isEmpty()) {
-            throw new EnderecoInvalidoException(mensagemException);
-        }
-    }
-    private void validaBairro(String campo, String mensagemException) {
-        if (isNull(campo) || campo.isEmpty()) {
-            throw new EnderecoInvalidoException(mensagemException);
-        }
-    }
-    private void validaCidade(String campo, String mensagemException) {
-        if (isNull(campo) || campo.isEmpty()) {
             throw new EnderecoInvalidoException(mensagemException);
         }
     }
